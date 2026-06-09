2fbd481a1786e27eb5f0a9babc971412a5e59733
Luis Enrique Palacio Dimas <palaciodimasluisenrique@gmail.com>
2026-06-08 19:23:24 -0500

    fix(hotel-configurations): add partial unique index to ignore soft-deleted records

--- app/Modules/Hotel/Infrastructure/Repositories/HotelConfigurationRepository.php
+++ app/Modules/Hotel/Infrastructure/Repositories/HotelConfigurationRepository.php
@@ -69,7 +69,7 @@ class HotelConfigurationRepository implements HotelConfigurationRepositoryInterf
         string $accommodationId,
         ?string $ignoreId = null
     ): bool {
-        $query = EloquentHotelConfiguration::withTrashed()
+        $query = EloquentHotelConfiguration::query()
             ->where('hotel_id', $hotelId)
             ->where('room_type_id', $roomTypeId)
             ->where('accommodation_id', $accommodationId);
